49a43d20c5cb8f80f9cef03cbc96b6345664c7b4
Brittney Estes <bnestes2014@gmail.com>
2019-10-25 15:47:27 -0400

    Add accessibility feature to word count.

--- charCount.js
+++ charCount.js
@@ -1,14 +1,26 @@
-let wordCountMessage = document.querySelector('#word-count');
-let charCountMessage = document.querySelector('#character-count');
-let textbox = document.querySelector('#text');
+let wordCount = document.querySelector('#word-count');
+let charCount = document.querySelector('#character-count');
+let textBox = document.querySelector('#text');
+let wordText = document.querySelector('#word-text');
 
 function charsAndWordsCount() {
-        wordCountMessage.textContent = textbox.value.trim().split(/\s+/)
-                                                           .filter(function(text) {
-                                                                return text !== "";
-                                                                })
-                                                           .length;
-        charCountMessage.textContent = textbox.value.length;
+        
+        // Set the counts for words and characters
+        wordCount.textContent = textBox.value.trim()
+                                             .split(/\s+/)
+                                             .filter(function(text) {
+                                                return text !== "";
+                                             })
+                                             .length;
+                                             
+        charCount.textContent = textBox.value.length;
+        
+        // If only one word is typed in textarea, make 'word' singular
+        if (wordCount.textContent > 0 && wordCount.textContent < 2) {
+                wordText.textContent = wordCount.textContent + ' word'
+        } else {
+                wordText.textContent = wordCount.textContent + ' words';
+        }
 }
 
-textbox.addEventListener('input', charsAndWordsCount, false);
+textBox.addEventListener('input', charsAndWordsCount, false);
